022d7427af87bab865a879716fffb6556faa7c46
Esteban Rios-Ovalle <estebanriosovalle@gmail.com>
2024-02-07 21:06:04 -0500

    put on hold

--- v1/scripts/preventcloser.js
+++ v1/scripts/preventcloser.js
@@ -1,7 +1,1 @@
 //so teachers cant close lmaoo L school
-window.addEventListener('beforeunload', function (e) {
-    // cancel close
-    e.preventDefault();
-    // put tha mesasge
-    e.returnValue = 'Are you sure you want to leave? Your changes may not be saved.';
-});
