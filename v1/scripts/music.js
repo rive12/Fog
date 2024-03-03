1abdcb2a575a3d37071673325611e2d7f2ca2b05
RiveDev <128239680+rive12@users.noreply.github.com>
2024-03-02 23:15:34 -0500

    um excuse me sir

---
+++ v1/scripts/music.js
@@ -0,0 +1,23 @@
+//spotify
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const spotifyOption = document.getElementById('spotifyOption');
+    spotifyOption.addEventListener('click', function() {
+        const iframeContainer = document.getElementById('iframeContainer');
+        if (iframeContainer && iframeContainer.src === 'https://bg.brolawncare.xyz/uv/service/hvtrs8%2F-mwskc%2Cymuvu%60e%2Ccmm-') {
+            iframeContainer.src = 'https://damarion-is-best.onrender.com/mathematics/https%3A%2F%2Fopen.spotify.com%2F';
+        }
+    });
+});
+
+//soundcloud
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const spotifyOption = document.getElementById('spotifyOption');
+    spotifyOption.addEventListener('click', function() {
+        const iframeContainer = document.getElementById('iframeContainer');
+        if (iframeContainer && iframeContainer.src === 'https://bg.brolawncare.xyz/uv/service/hvtrs8%2F-mwskc%2Cymuvu%60e%2Ccmm-') {
+            iframeContainer.src = 'https://bg.brolawncare.xyz/uv/service/hvtrs8%2F-smuldalmuf.aoo%2F';
+        }
+    });
+});
